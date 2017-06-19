93579e95d87ea436aacd8de2d34679bc19f994b6
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-06-19 13:12:35 -0400

    wip: initial tempaltes and test data for messages

--- app/tests/views/test_users_messages.py
+++ app/tests/views/test_users_messages.py
@@ -66,31 +66,6 @@ class UserNewMessageRoutingAndHttpTests(TestCase, RouteTestingWithKwargs):
         self.kwargs = {'user_id': 55}
 
 
-# Tests checking that that '/users/:user_id/messages/message_id/edit' properly handles HttpRequests and routing
-# Accepts GET requests and refuses all others with an error code 405 (Method not allowed)
-# Tested on user_id 55 and message_id 22
-class UserEditMessageRoutingAndHttpTests(TestCase, RouteTestingWithKwargs):
-    # setup for all test cases
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.client = Client()
-        self.route_name = 'app:edit_user_message'
-        self.route = '/users/55/messages/22/edit'
-        self.view = messages.edit_user_message
-        self.responses = {
-            'exists': 200,
-            'GET': 200,
-            'POST': 405,
-            'PUT': 405,
-            'PATCH': 405,
-            'DELETE': 405,
-            'HEAD': 405,
-            'OPTIONS': 405,
-            'TRACE': 405
-        }
-        self.kwargs = {'user_id': 55, 'message_id': 22}
-
-
 # Tests checking that that '/users/:user_id/messages/message_id' properly handles HttpRequests and routing
 # Accepts GET, PATCH, PUT, and DELETE requests and refuses all others with an error code 405 (Method not allowed)
 # Tested on user_id 55 and message_id 22
--- app/views/users/messages/views.py
+++ app/views/users/messages/views.py
@@ -2,22 +2,27 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect
 from django.template.loader import get_template
 
+from app.models import Message
+from app.models import User
 
 @require_http_methods(["GET", "POST"])
 def user_messages(request, user_id):
-    return HttpResponse(str(user_id) + "Messages Index!")
+    current_user = User(email="tests@example.com", first_name="Tester", last_name="McTestsalot")
+    messages = []
+    message = Message(message="This is a test message")
+    return render(request, "users/messages/index.html", {
+        'current_user': current_user,
+        'messages': messages,
+        'message': message
+    })
 
 
 @require_http_methods(["GET"])
 def new_user_message(request, user_id):
     return HttpResponse(str(user_id) + "New Message!")
-
-
-@require_http_methods(["GET"])
-def edit_user_message(request, user_id, message_id):
-    return HttpResponse("edit user message" + str(user_id) + str(message_id))
 
 
 @require_http_methods(["GET", "PATCH", "PUT", "DELETE"])
